3b2255b5614a699b7874c8494d96fb641fc94c41
WXH <2758705328@qq.com>
2023-05-06 23:14:46 +0800

    my commit

---
+++ DynamicPicture/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace DynamicPicture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(28, -6);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(551, 800);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(613, 88);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "种草";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(613, 149);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 38);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "猫耳朵";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(648, 248);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(75, 79);
+            this.axWindowsMediaPlayer1.TabIndex = 3;
+            this.axWindowsMediaPlayer1.Enter += new System.EventHandler(this.axWindowsMediaPlayer1_Enter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1119, 794);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+    }
+}
+
---
+++ DynamicPicture/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DynamicPicture
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+           
+        }
+        Image imaGirl;
+        Image[] imaGrass;
+        Image[] imaCircle;
+        Image[] imaEar;
+        Image[] imaMoustache;
+        int idx;
+       
+        public enum EffectStyle { None,Grass,CatEar};
+
+        EffectStyle effect = new EffectStyle();
+
+
+
+        /// <summary>
+        /// 资源载入
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            imaGirl = Image.FromFile("girl2.jpeg");
+            imaGrass = new Image[61];
+            imaCircle = new Image[61];
+            imaEar = new Image[80];
+            imaMoustache = new Image[80];
+            for(int i = 0;i < 61; i++)
+            {
+                imaGrass[i] = Image.FromFile("res/grass/grass_" + i.ToString("D3")+".png");
+                imaCircle[i] = Image.FromFile("res/yuan/yuan_" + i.ToString("D3") + ".png");
+            }
+            for(int i = 0;i < 80; i++)
+            {
+                imaEar[i] = Image.FromFile("res/ear/ear_" + i.ToString("D3") + ".png");
+                imaMoustache[i] = Image.FromFile("res/moustache/moustache_" + i.ToString("D3") + ".png");
+            }
+            timer1.Enabled = true;
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            g.DrawImage(imaGirl, 0, 0);
+
+            if(effect==EffectStyle.Grass) //种草
+            {
+                g.DrawImage(imaGrass[idx%61], 65, -20);
+                g.DrawImage(imaCircle[idx % 61], 130, 200);
+            }
+            else if(effect == EffectStyle.CatEar) //猫耳朵
+            {
+                g.DrawImage(imaEar[idx % 80], 70, 0);
+                g.DrawImage(imaMoustache[idx % 80], 52, 175);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            idx++;
+
+            pictureBox1.Invalidate();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            effect = EffectStyle.CatEar;
+            idx = 0;
+            axWindowsMediaPlayer1.URL = "res/sound/dance_with_my_love.mp3";
+            axWindowsMediaPlayer1.settings.setMode("loop", true);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            effect = EffectStyle.Grass;
+            idx = 0;
+        }
+
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
